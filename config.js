acbf40960607fb1a8834ff3e7eeb9e1dbba09ff4
Niko Salonen <niko.salonen@almamedia.fi>
2018-10-23 14:39:00 +0300

    linted config

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  server: 'open.ircnet.net',
+  nickname: 'varavibe',
+  channels: [ 'rölö' ],
+  username: 'varavibe',
+  realName: ';ASD;A;SD;A',
+  autoRejoin: true,
+  autoConnect: true
+};
