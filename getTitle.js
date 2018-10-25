9437c5488a4d58ee2c43de92c9b0f2eb5257064e
Niko Salonen <niko.salonen@almamedia.fi>
2018-10-25 08:24:03 +0300

    Add more headers for scrape

--- getTitle.js
+++ getTitle.js
@@ -13,10 +13,12 @@ function getTitle(url) {
     url: url,
     jar: request.jar(),
     headers: {
+      'Content-Type':'text/html; charset=utf-8',
+      'Accept-Language': 'en-US',
       'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
     }
   };
-  let message =  scrape(options);
+  let message = scrape(options);
   return message;
 }
 exports.getTitle = getTitle;
